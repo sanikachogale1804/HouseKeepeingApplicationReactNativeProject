13db8aa336611a6f31811c07eba802411830f37a
unknown <https://github.com/sanikachogale1804.com>
2025-11-05 09:50:00 +0530

    5-nov-2025

--- admin-panel/src/Components/Config/apiconfig.jsx
+++ admin-panel/src/Components/Config/apiconfig.jsx
@@ -2,9 +2,9 @@
 const Api_link = (() => {
   const hostname = window.location.hostname;
 
-  if (hostname === "localhost") return "https://localhost:5005"; 
-  if (hostname === "192.168.1.92") return "https://192.168.1.92:5005";
+  if (hostname === "localhost") return "http://localhost:5005"; 
+  if (hostname === "192.168.1.92") return "http://192.168.1.92:5005";
   // deployment: use full URL with port
-  return "https://45.115.186.228:5005";
+  return "http://45.115.186.228:5005";
 })();
 export default Api_link;
